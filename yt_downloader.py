c04925cb9a2954000d80bdda867cd0e9eab93ed5
Gabriel Ambrosio Fernandez <127698444+gabigogunner@users.noreply.github.com>
2026-09-08 12:29:07 -0300

    Create yt_downloader.py

---
+++ yt_downloader.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from yt_dlp import YoutubeDL
+
+st.title("Baixador de vídeos do Youtube (liste todos os vídeos por linha)")
+video_txt_area = st.text_area("Link(s) do(s) vídeo(s)", height=200)
+URLS = video_txt_area.split("\n")
+formato_escolha = st.radio("Escolha o formato:", ["MP4 (Vídeo Melhor Qualidade)", "MP3 (Apenas Áudio)"])
+baixar_button = st.button("Baixar")
+
+
+if baixar_button:
+    formatacao = {}
+    if formato_escolha == "MP4 (Vídeo Melhor Qualidade)":
+        formatacao = {
+            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best', # Apenas a palavra best, sem símbolos ou barras
+            'outtmpl': '%(title)s.%(ext)s',
+            'cookiesfrombrowser': ('chrome', ), # se estiver usando cookies
+            'js_runtimes': {'node': {}},          # Força o yt-dlp a usar o Node.js
+            'remote_components': ['ejs:github'],
+        }
+        
+    elif formato_escolha == "MP3 (Apenas Áudio)":
+        formatacao = {
+            "format":"bestaudio/best",
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192', # Qualidade do mp3 (192kbps)
+            }],
+            "outtmpl":"%(title)s.%(ext)s",
+            'cookiesfrombrowser': ('chrome', ), # se estiver usando cookies
+            'js_runtimes': {'node': {}},          # Força o yt-dlp a usar o Node.js
+            'remote_components': ['ejs:github'],
+        }
+    try:
+        with YoutubeDL(formatacao) as ydl:
+            ydl.download(URLS)
+            baixar_button = False        
+            st.success("Download concluído com sucesso!")
+        
+    except Exception as e:
+        st.error(f"Ocorreu um erro durante o download: {e}")
